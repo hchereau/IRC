99dfa462b6ab15426e028d54bb98ad04efbe09f2
Hucherea <hcehreau42@gmail.com>
2026-03-25 11:04:16 +0100

    class Client created and basic variables added

---
+++ includes/Client.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Client {
+	public:
+
+	private:
+
+		std::string	_readBuffer;
+		std::string	_writeBuffer;
+		std::string	_Nickname;
+		std::string	_Username;
+		std::string	_Realname;
+		std::string	_statut;
+};

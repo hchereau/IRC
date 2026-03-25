8efdc3aeca7ac60b49fddbb9b0e5005a5e9f9814
Hucherea <hcehreau42@gmail.com>
2026-03-25 13:26:23 +0100

    fix main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ void printTestResult(const std::string& testName, bool condition) {
     if (condition) {
         std::cout << "[\033[32mOK\033[0m] " << testName << std::endl;
     } else {
-        std::cout << "[\033[31mKO\033[0m] " << testName << std::endl;
+        std::cerr << "[\033[31mKO\033[0m] " << testName << std::endl;
     }
 }
 

7516d4ff0d97fe68e9304a9cbd86e348cc10ab33
Hucherea <hcehreau42@gmail.com>
2026-04-06 15:29:01 +0200

    fix test

--- objs/srcs/main.d
+++
@@ -1,7 +0,0 @@
-objs/srcs/main.o: srcs/main.cpp includes/Server.hpp includes/Client.hpp \
- includes/Channel.hpp includes/Parsing.hpp includes/Executor.hpp
-includes/Server.hpp:
-includes/Client.hpp:
-includes/Channel.hpp:
-includes/Parsing.hpp:
-includes/Executor.hpp:

3213a9acdca81de0d88ea002b3ea80a0b3d6bc41
Hucherea <hcehreau42@gmail.com>
2026-04-11 01:13:04 +0200

    remove objs

--- objs/srcs/commands/cmd_kick.d
+++
@@ -1,9 +0,0 @@
-objs/srcs/commands/cmd_kick.o: srcs/commands/cmd_kick.cpp \
- includes/Executor.hpp includes/Client.hpp includes/Channel.hpp \
- includes/Parsing.hpp includes/Server.hpp includes/Replies.hpp
-includes/Executor.hpp:
-includes/Client.hpp:
-includes/Channel.hpp:
-includes/Parsing.hpp:
-includes/Server.hpp:
-includes/Replies.hpp:
--- objs/srcs/commands/cmd_user.d
+++
@@ -1,9 +0,0 @@
-objs/srcs/commands/cmd_user.o: srcs/commands/cmd_user.cpp \
- includes/Executor.hpp includes/Client.hpp includes/Channel.hpp \
- includes/Parsing.hpp includes/Server.hpp includes/Replies.hpp
-includes/Executor.hpp:
-includes/Client.hpp:
-includes/Channel.hpp:
-includes/Parsing.hpp:
-includes/Server.hpp:
-includes/Replies.hpp:
